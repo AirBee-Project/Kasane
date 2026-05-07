d6d5bfcc3870ca250d68cd78dc0ad152faf23bb2
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-05-07 14:13:16 +0900

    ZoomLevelPolicyを追加

--- src/models/layer/data/request.rs
+++ src/models/layer/data/request.rs
@@ -1,11 +1,13 @@
+use crate::models::query::Query;
 use serde::Deserialize;
 use utoipa::ToSchema;
-use crate::models::query::Query;
 
 #[derive(Debug, Deserialize, ToSchema)]
 /// 空間IDの範囲を[Query]で指定して値を取得する
 pub struct GetDataRequest {
     pub query: Query,
+    #[serde(default)]
+    pub zoom_level_policy: ZoomLevelPolicy,
 }
 
 #[derive(Debug, Deserialize, ToSchema)]
@@ -13,10 +15,26 @@ pub struct GetDataRequest {
 pub struct InsertDataRequest {
     pub query: Query,
     pub value: serde_json::Value,
+    #[serde(default)]
+    pub zoom_level_policy: ZoomLevelPolicy,
 }
 
 #[derive(Debug, Deserialize, ToSchema)]
 /// 空間IDの範囲を[Query]で指定して値を削除する
 pub struct RemoveDataRequest {
     pub query: Query,
+    #[serde(default)]
+    pub zoom_level_policy: ZoomLevelPolicy,
+}
+
+#[derive(Debug, Deserialize, ToSchema, Default, Clone, Copy, PartialEq, Eq)]
+/// 各Layerのmax_zoom_levelよりも小さなIDが入力された場合の挙動を設定できる
+pub enum ZoomLevelPolicy {
+    #[default]
+    ///エラーを返す
+    Error,
+    ///そのIDを無視する
+    Ignore,
+    ///そのIDを含むmax_zoom_levelのIDに正規化する
+    Normalize,
 }
