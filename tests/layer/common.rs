6833c8f74dbe0eec56832e1832dc97e69a6c07de
Tomoro Saito <s.tomoro0726@gmail.com>
2026-05-09 17:52:00 +0900

    テストをいくつか追加

--- tests/layer/common.rs
+++ tests/layer/common.rs
@@ -11,7 +11,6 @@ use tower::ServiceExt;
 
 pub struct TestApp {
     pub app: Router,
-    // temp_fileを保持しておくことで、テスト終了時までDBファイルを維持する
     _temp_file: NamedTempFile,
 }
 
@@ -21,14 +20,13 @@ impl TestApp {
         let db = db_init::initialize_database(temp_file.path().to_str().unwrap());
         let app_state = AppState { redb: Arc::new(db) };
         let app = kasane(app_state);
-
         Self {
             app,
             _temp_file: temp_file,
         }
     }
 
-    /// テスト用にレイヤーを初期作成するヘルパーメソッド
+    /// テスト用にレイヤーを初期作成する
     pub async fn create_layer(&self, name: &str, data_type: &str, max_zoom_level: u8) {
         let create_body = serde_json::json!({
             "name": name,
