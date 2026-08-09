2eceeb739a937c4c4e9983df4d8ff264a723174d
Tomoro Saito <s.tomoro0726@gmail.com>
2026-08-09 22:48:37 +0900

    Serversを削除

--- src/openapi.rs
+++ src/openapi.rs
@@ -61,9 +61,6 @@ const PRIVILEGE_LEGEND: &str = r#"
         description = PRIVILEGE_LEGEND,
         license(name = "MIT", url = "https://opensource.org/licenses/MIT")
     ),
-    servers(
-        (url = "/", description = "Local server")
-    ),
     paths(
         // Auth
         crate::handlers::auth::login,
